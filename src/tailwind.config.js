4cb918077ad5f4d96d08d035a3c38b5b9de02a50
ZerohRaG3 <kyledickey02@gmail.com>
2023-06-30 16:07:44 -0600

    tailwind init

---
+++ src/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./comps/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    colors: {
+      pink: {
+        50: '#fcd2ff',
+        100: '#fcc9ff',
+        200: '#fbc0ff',
+        300: '#fbb7ff',
+        400: '#faaeff',
+        500: '#faa6ff',
+        600: '#e195e5',
+        700: '#c884cc',
+        800: '#af74b2',
+        900: '#966399'
+      }
+    }
+  },
+  plugins: [],
+}
+
