a0e8650875939eb656b92885317becfb84d9d9cd
Kyle Dickey <41174949+dickeyy@users.noreply.github.com>
2024-01-28 16:48:25 -0700

    fix images

--- new/comps/productPictures/index.tsx
+++ new/comps/productPictures/index.tsx
@@ -1,10 +1,10 @@
 export default function ProductPictures(props: any) {
   return (
-    <div className="flex flex-row justify-start items-center -mt-2 flex-wrap">
+    <div className="flex flex-row justify-start items-center -mt-2 flex-wrap gap-2">
       {props.pictures.map((image: any) => (
         <img
           src={image.node.originalSrc}
-          className="w-16 h-16 rounded-lg mr-2 border border-zinc-700 cursor-pointer hover:opacity-80 transition-all ease-in-out duration-150"
+          className="w-16 h-16 rounded-lg border border-zinc-700 cursor-pointer hover:opacity-80 transition-all ease-in-out duration-150"
           onClick={() => props.setCurrentPicture(image.node.originalSrc)}
         />
       ))}
