12949b22e9fc385955d0b16bc057be85b862ce93
Kyle Dickey <41174949+dickeyy@users.noreply.github.com>
2024-02-22 14:35:00 -0700

    begging to the lord

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -11,13 +11,16 @@ async function getProducts() {
 
 export default async function ProductList() {
     const products: GenralProductType[] = await getProducts();
+    if (!products) return null;
 
     return (
         <div id="shop" className="flex w-full flex-col items-start justify-start gap-4">
             <h1 className="text-3xl font-semibold">All Products</h1>
             <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
                 <Suspense fallback={<Skeleton className="h-12 w-12"></Skeleton>}>
-                    {products?.map((product) => <Product key={product?.id} product={product} />)}
+                    {products.map((product) => (
+                        <Product key={product.id} product={product} />
+                    ))}
                 </Suspense>
             </div>
         </div>
