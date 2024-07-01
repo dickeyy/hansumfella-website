4b2b62b1a12ed83e915c173c5213273311cc5f1a
Kyle Dickey <k@kyle.so>
2024-07-01 08:54:32 -0600

    hero and landing

---
+++ src/components/TwitchEmbed.tsx
@@ -0,0 +1,5 @@
+import { TwitchPlayerNonInteractive } from "react-twitch-embed";
+
+export default function TwitchEmbed() {
+    return <TwitchPlayerNonInteractive channel="hansumfella" className="w-full rounded-lg" />;
+}
